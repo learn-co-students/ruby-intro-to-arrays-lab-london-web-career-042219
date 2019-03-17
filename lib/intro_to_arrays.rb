813f57a5bd2f9e3b9f42f0b3597c5b7ad1f9995f
Daniel Bovington <d.bovington@gmail.com>
2019-03-17 18:28:44 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+ []
+end
+
+def array_with_two_elements
+  dudes = ["me", "myself"]
+end
+
+def first_element(dudes)
+  dudes[0]
+end
+
+def third_element(dudes)
+  dudes[2]
+end
+
+def last_element(dudes)
+  dudes[-1]
+end
+
+def first_element_with_array_methods(dudes)
+  dudes.first
+end
+
+def last_element_with_array_methods(dudes)
+  dudes.last
+end
+
+def length_of_array(dudes)
+  dudes.length
+end
